8fed1f2cd1bbde774932e11820a754eb551fdc7c
rebecca maria <rebecca@bigum.dk>
2025-09-04 13:58:44 +0200

    Lunarlander

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
-int main() {return 42;}
+#include <stdio.h>
+
+int Control(int altitude) {
+  int thruster = 0;
+if (altitude<=100 && altitude>0){
+    thruster=1;
+}else {
+    thruster=0;
+}
+
+  return thruster;
+}
+
+void Test(int altitude) {
+  int thruster = Control(altitude);
+  int behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                        (altitude <= 100 && altitude > 0 && thruster == 1) ||
+                        (altitude <= 0 && thruster == 0);
+  char *behaviorCorrectIcon = behaviorCorrect ? "✅" : "❌";
+  printf("For altitude %3d, your thruster is %d |%s|\n", altitude, thruster,
+         behaviorCorrectIcon);
+}
+
+int main(void) {
+  Test(150);
+  Test(100);
+  Test(50);
+  Test(0);
+  Test(-1);
+}
